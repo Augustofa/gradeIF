1246564cab6bfc41ab4e886f023cc349aa5468f5
Augustofa <augusto.fer.arantes@gmail.com>
2025-06-16 15:36:54 -0300

    ajustes de estilização e responsividade mobile

--- src/main/resources/static/js/nav-func.js
+++ src/main/resources/static/js/nav-func.js
@@ -22,11 +22,36 @@ document.addEventListener("DOMContentLoaded", function () {
 function toggleNav() {
     var sidebar = document.getElementById("sidebar");
     var main = document.getElementById("main");
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0";
-        main.style.marginLeft = "0";
+
+    if (window.innerWidth >= 992) {
+        if (sidebar.style.width === "250px" || sidebar.style.width === "") {
+            sidebar.style.width = "0";
+            main.style.marginLeft = "0";
+        } else {
+            sidebar.style.width = "250px";
+            main.style.marginLeft = "250px";
+        }
     } else {
-        sidebar.style.width = "250px";
-        main.style.marginLeft = "250px";
+        if (sidebar.style.width === "250px" || sidebar.style.width === "") {
+            sidebar.style.width = "0";
+            sidebar.classList.remove("active");
+        } else {
+            sidebar.style.width = "250px";
+            sidebar.classList.add("active");
+        }
     }
 }
+
+document.addEventListener('click', function(event) {
+    var sidebar = document.getElementById("sidebar");
+    var navbarToggle = document.getElementById("navbar-toggle-button");
+
+    if (window.innerWidth < 992 &&
+        sidebar.classList.contains("active") &&
+        !sidebar.contains(event.target) &&
+        event.target !== navbarToggle &&
+        !navbarToggle.contains(event.target)) {
+        sidebar.style.width = "0";
+        sidebar.classList.remove("active");
+    }
+});
