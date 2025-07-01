689de08ae213dffb96ec4b36d408628f8f5c45b6
augustofa <augusto.fer.arantes@gmail.com>
2025-07-01 20:19:59 -0300

    ajuste de luminância na grade-aluno

--- src/main/resources/static/js/montagem-grade.js
+++ src/main/resources/static/js/montagem-grade.js
@@ -7,3 +7,13 @@ function addDisciplina() {
         alert('Por favor, selecione uma disciplina.');
     }
 }
+
+function calcCorContraste(bgHex){
+    let r = parseInt(bgHex.substring(1, 3), 16);
+    let g = parseInt(bgHex.substring(3, 5), 16);
+    let b = parseInt(bgHex.substring(5, 7), 16);
+
+    let luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+    return luminance > 0.5 ? "#000000" : "#FFFFFF";
+}
